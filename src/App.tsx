d0a8c3eebb064d5b436db6eaf518b9ec8ef291e3
Stephen Buchanan <stephen@pop-os.localdomain>
2019-08-26 16:35:01 +0200

    dummy data in separate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,8 @@
 import React from 'react';
 import './App.scss';
 import Timeline, { ITimeline, IVector2, ITimelinePlayback } from './Timeline/Timeline';
-import Defaults from './Defaults';
-
-
-const dummy = {
-  tracks: [
-    {
-      id: 0,
-      name: "T1",
-      blocks: [
-        {
-          id: 0,
-          name: "One",
-          start: 0,
-          duration: 1
-        },
-        {
-          id: 1,
-          name: "Two",
-          start: 2,
-          duration: 2
-        }
-      ]
-    },
-    {
-      id: 1,
-      name: "T2",
-      blocks: [
-        {
-          id: 0,
-          name: "Three",
-          start: 0.5,
-          duration: 3
-        },
-        {
-          id: 1,
-          name: "Four",
-          start: 3.5,
-          duration: 5
-        }
-      ]
-    }    
-  ]
-} as ITimeline;
+import Defaults from './Data/Defaults';
+import * as Dummy from './Data/Dummy';
 
 interface IAppState {
   timeline: ITimeline,
@@ -56,7 +15,7 @@ interface IAppState {
 class App extends React.Component<any, IAppState> {
 
   state = {
-    timeline: dummy,
+    timeline: Dummy.dummy1,
     scale: Defaults.scale
   } as IAppState
 
---
+++ src/Data/Dummy.tsx
@@ -0,0 +1,42 @@
+import { ITimeline } from "../Timeline/Timeline";
+
+export const dummy1 = {
+  tracks: [
+    {
+      id: 0,
+      name: "T1",
+      blocks: [
+        {
+          id: 0,
+          name: "One",
+          start: 0,
+          duration: 1
+        },
+        {
+          id: 1,
+          name: "Two",
+          start: 2,
+          duration: 2
+        }
+      ]
+    },
+    {
+      id: 1,
+      name: "T2",
+      blocks: [
+        {
+          id: 0,
+          name: "Three",
+          start: 0.5,
+          duration: 3
+        },
+        {
+          id: 1,
+          name: "Four",
+          start: 3.5,
+          duration: 5
+        }
+      ]
+    }    
+  ]
+} as ITimeline;
